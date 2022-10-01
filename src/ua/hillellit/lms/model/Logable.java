0217473ee3cfc11f71fe97d18d08cd05ab6cb9bb
LudmilaLitvinova <ludik-1984@ukr.net>
2022-10-01 18:28:24 +0300

    Refactor code

--- src/ua/hillellit/lms/model/Logable.java
+++ src/ua/hillellit/lms/model/Logable.java
@@ -1,9 +1,11 @@
 package ua.hillellit.lms.model;
 
 import java.io.IOException;
+import ua.hillellit.lms.exceptions.FileMaxSizeReachedException;
 
 public interface Logable {
 
-  void log(String message) throws IOException;
+  public void debug(String message) throws FileMaxSizeReachedException, IOException;
+  public void info(String message) throws FileMaxSizeReachedException, IOException;
 
 }
